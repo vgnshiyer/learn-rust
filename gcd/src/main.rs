fb26e8ec6c2e52f1878e1ef5e7ca8cd4cefcc718
vgnshiyer <vgnshiyer@gmail.com>
2024-03-01 23:20:58 -0700

    chore: gcd of two numbers in rust

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -1,5 +1,5 @@
-use std::env;
-use std::str::FromStr;
+use std::env; // env module provides several useful functions
+use std::str::FromStr; // bring trait (collection of methods) FromStr into scope
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0); // panic if n or m is 0
@@ -14,7 +14,28 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 
-fn main() {}
+fn main() {
+    let mut numbers = Vec::new(); // vector --> growable datatype
+
+    for arg in env::args().skip(1) {
+        // the u64::from_str() method does not directly return a u64, but a Result
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+    // Result has two values (Ok and Err) -> the expect method prints the message on Err and simply returns the parsed value on success (Ok)
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ..."); // eprintln! macro writes an error to std output
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        // & --> borrows a reference of the elements in numbers (ownership of the elements stay with numbers)
+        d = gcd(d, *m); // * --> dereference the value stored in the address
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
 
 // attribute -> marking functions and declarations with extra information (just like attributes in C# or annotations in Java)
 #[test] // skipped by normal cargo build, but run by cargo test
