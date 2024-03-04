20ac45039539fc124d7ff2ad49a53dc5ea0e1c24
vgnshiyer <vgnshiyer@gmail.com>
2024-03-03 23:35:03 -0700

    quick find & replace program

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,4 +1,6 @@
+use regex::Regex;
 use std::env;
+use std::fs;
 use text_colorizer::*;
 
 #[derive(Debug)] // tells compiler to write extra code so that we can print struct with {:?}
@@ -38,7 +40,45 @@ fn parse_args() -> Arguments {
     }
 }
 
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
+
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    }
 }
