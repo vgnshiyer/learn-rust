7f80769eee9f6fa8f28d9743b8ebb173a87fe3af
vgnshiyer <vgnshiyer@gmail.com>
2024-03-03 21:19:07 -0700

    quickreplace program

---
+++ quickreplace/src/main.rs
@@ -0,0 +1,44 @@
+use std::env;
+use text_colorizer::*;
+
+#[derive(Debug)] // tells compiler to write extra code so that we can print struct with {:?}
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
+}
+
+fn print_usage() {
+    eprintln!(
+        "{} - change occurrances of one string into another",
+        "quickreplace".green()
+    );
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}.",
+            "Error:".red().bold(),
+            args.len()
+        );
+        std::process::exit(1);
+    }
+
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
+}
+
+fn main() {
+    let args = parse_args();
+    println!("{:?}", args);
+}
