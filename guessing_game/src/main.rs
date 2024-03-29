95769d63dd131e5426aa3fcbefe23f793719af7a
vgnshiyer <vgnshiyer@gmail.com>
2024-03-29 09:59:16 -0700

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,35 +1,41 @@
-use rand::Rng; // rand is a crate, Rng is a trait
-use std::cmp::Ordering; // std is a crate, cmp is a module, Ordering is an enum
-use std::io; // std is a crate, io is a module
+use std::{cmp::Ordering, io};
+use rand::{thread_rng, Rng};
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the secret number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    #[allow(unused_variables)] // there are called compiler attributes that apply to methods, functions & variables
+    let secret: u32 = thread_rng().gen_range(1..=100);
 
     loop {
-        println!("Please input your guess.");
-
+        println!("Make a guess!");
         let mut guess = String::new();
 
+        // :: --> call a function in a module
+        // . --> call a method in a class(struct)
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read line");
-
+            .expect("Enter a valid number");
+    
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
+            Err(e) => {
+                println!("Parse Error: {e}");
                 break;
-            }
+            },
+        };
+    
+        println!("Your guess {guess}");
+    
+        // match is a powerful version of if/else (handles more complex code in a concise manner than if/else)
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You are right!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!"),
         }
     }
+
 }
