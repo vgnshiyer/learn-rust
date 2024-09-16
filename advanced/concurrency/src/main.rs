1eb6e242e4143ed5b5fc63c0d51e4cbb0b1e0a6b
Vignesh Iyer <vgnshiyer@asu.edu>
2024-09-15 20:45:01 -0700

    mutexes

--- advanced/concurrency/src/main.rs
+++ advanced/concurrency/src/main.rs
@@ -1,14 +1,18 @@
-use std::{thread, sync::Arc};
+use std::{sync::{Arc, Mutex}, thread};
 
 fn main() {
     // to share data across multiple thread, use Arc
     // Arc is a shared pointer -> has some overhead (reference counter) compared to standard pointer
-    let data = Arc::new(vec![1, 2, 3, 4, 5]);
+    // Arc itself lets you only use the data across threads, but they are still immutable
+    // we make them mutable with Mutex
+    let data = Arc::new(Mutex::new(vec![1, 2, 3, 4, 5]));
     let mut handles = vec![];
     for i in 0..3 {
         let data_clone = Arc::clone(&data);
         let handle = thread::spawn(move || {
-            println!("Print data in thread {} {:?}", i, data_clone);
+            let mut data = data_clone.lock().unwrap(); // lock the data by a thread
+            data.push(i);
+            // the lock is realeased here
         });
         handles.push(handle);
     }
@@ -18,5 +22,5 @@ fn main() {
         handle.join().unwrap(); // wait for threads to complete
     }
 
-    println!("Print data in main: {:?}", data);
+    println!("Print data in main: {:?}", data.lock().unwrap());
 }
