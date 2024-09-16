2b8d7a13f92ef47c23c7fed299428f89e35d28a1
Vignesh Iyer <vgnshiyer@asu.edu>
2024-09-15 20:58:11 -0700

    read write locks

--- advanced/concurrency/src/main.rs
+++ advanced/concurrency/src/main.rs
@@ -1,4 +1,4 @@
-use std::{sync::{Arc, Mutex}, thread};
+use std::{sync::{Arc, Mutex, RwLock}, thread, time::Duration};
 
 fn main() {
     // to share data across multiple thread, use Arc
@@ -23,4 +23,40 @@ fn main() {
     }
 
     println!("Print data in main: {:?}", data.lock().unwrap());
+
+    println!("");
+    println!("------------------------------------------------");
+    println!("");
+
+    // we can use RwLock to have read and write locks on the data
+    // threads can simultaneously read without locks, but the write has to happen with a lock
+    let data = Arc::new(RwLock::new(vec![1, 2, 3, 4, 5]));
+    let mut handles = vec![];
+    for i in 0..3 {
+        let data_clone = Arc::clone(&data);
+        let handle = thread::spawn(move || {
+            if i == 2 {
+                let mut data = data_clone.write().unwrap(); // write lock -> everything has to stop
+                println!("Writing to data from thread {}", i);
+                data.push(i);
+                thread::sleep(Duration::from_secs(1));
+                println!("Done writing from thread {}", i)
+                // the write lock is released here
+            } else {
+                // simulataneous read
+                let data = data_clone.read().unwrap();
+                println!("Reading data in thread {} {:?}", i, data);
+                thread::sleep(Duration::from_secs(1));
+                println!("Done reading data from thread {}", i);
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap(); // wait for threads to complete
+    }
+
+    println!("Print data in main: {:?}", data.read().unwrap());
+
 }
